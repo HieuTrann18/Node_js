6a53e9ea83868f3c48f0f048cfb46a4eedfc86c4
HieuTrann18 <hieu.tt.2455@aptechlearning.edu.vn>
2025-03-31 03:38:15 +0700

    update

--- src/main.js
+++ src/main.js
@@ -9,13 +9,14 @@ const port = 3001;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP Logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 // Template Engine
 app.engine('hbs', engine({
   extname: '.hbs'
 }))
 
 app.set('view engine', 'hbs');
+
 app.set('views', path.join(__dirname, 'resources\\views'));
 
 app.get('/', (req, res) => {
@@ -25,6 +26,11 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news')
+})
+
+app.get('/search', (req, res) => {
+
+  res.render('search')
 })
 
 
